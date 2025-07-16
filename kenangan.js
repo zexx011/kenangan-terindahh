bd0da35e9c80bca95463da7aa6378ba1f303ea9c
zexx011 <zyexxyz@gmail.com>
2025-07-16 22:03:35 +0700

    Add files via upload

---
+++ kenangan.js
@@ -0,0 +1,52 @@
+// Efek mengetik
+const teks = "Momen Tak Terlupakan...";
+let i = 0;
+function ketik() {
+  if (i < teks.length) {
+    document.getElementById("typing").innerHTML += teks.charAt(i);
+    i++;
+    setTimeout(ketik, 150);
+  }
+}
+ketik();
+
+// Bintang bergerak
+const canvas = document.getElementById('stars');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let stars = [];
+for (let i = 0; i < 100; i++) {
+  stars.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    r: Math.random() * 1.5,
+    d: Math.random() * 1
+  });
+}
+
+function drawStars() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
+  ctx.beginPath();
+  for (let i = 0; i < stars.length; i++) {
+    let s = stars[i];
+    ctx.moveTo(s.x, s.y);
+    ctx.arc(s.x, s.y, s.r, 0, Math.PI * 2, true);
+  }
+  ctx.fill();
+  moveStars();
+}
+
+function moveStars() {
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].y += stars[i].d;
+    if (stars[i].y > canvas.height) {
+      stars[i].y = 0;
+      stars[i].x = Math.random() * canvas.width;
+    }
+  }
+}
+
+setInterval(drawStars, 33);
